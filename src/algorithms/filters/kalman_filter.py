d5ca53030467a8f560c8ed8b85afdc372d454cb0
BauerJustin <justin.bauer@mail.utoronto.ca>
2023-10-24 20:35:13 -0400

    Implemented KF

--- src/algorithms/filters/kalman_filter.py
+++ src/algorithms/filters/kalman_filter.py
@@ -1,6 +1,33 @@
+import numpy as np
+from src import constants as const
+from src.utils import Position
+
 class KalmanFilter:
     def __init__(self):
-        pass
+        self.covariance = np.array(const.INITIAL_COVARIANCE)  # P
+        self.process_noise = np.array(const.PROCESS_NOISE)  # Q
+        self.measurement_noise = np.array(const.MEASUREMENT_NOISE)  # R
+        self.transition_matrix = np.array(const.TRANSITION_MATRIX)  # F
+        self.observation_matrix = np.array(const.OBSERVATION_MATRIX)  # H
+    
+    def update(self, pos, new_pos):
+        self.state = np.array(pos.state())
+        self._kf_predict()
+        self._kf_update(measurements=new_pos.unpack())
+        pos.update(*self.state)
 
-    def update(self):
-        pass
+    def _kf_predict(self):
+        # x(k) = F * x(k-1)
+        self.state = np.dot(self.transition_matrix, self.state)
+        # P(k) = F * P(k-1) * F^T + Q
+        self.covariance = np.dot(np.dot(self.transition_matrix, self.covariance), self.transition_matrix.T) + self.process_noise
+
+    def _kf_update(self, measurements):
+        # K = P(k) * H^T * (H * P(k) * H^T + R)^(-1)
+        kalman_gain = np.dot(np.dot(self.covariance, self.observation_matrix.T), 
+                             np.linalg.inv(np.dot(np.dot(self.observation_matrix, self.covariance), self.observation_matrix.T) + self.measurement_noise))
+
+        # x(k) = x(k) + K * (Z - H * x(k))
+        self.state = self.state + np.dot(kalman_gain, (measurements - np.dot(self.observation_matrix, self.state)))
+        # P(k) = (I - K * H) * P(k)
+        self.covariance = np.dot((np.eye(6) - np.dot(kalman_gain, self.observation_matrix)), self.covariance)
--- src/utils/position.py
+++ src/utils/position.py
@@ -1,9 +1,26 @@
 class Position:
-    def __init__(self, x=None, y=None, z=None):
+    def __init__(self, x=None, y=None, z=None, x_dot=0.0, y_dot=0.0, z_dot=0.0):
         self.x = x
         self.y = y
         self.z = z
+        self.x_dot = x_dot
+        self.y_dot = y_dot
+        self.z_dot = z_dot
 
     def unpack(self):
         return self.x, self.y, self.z
+    
+    def state(self):
+        return self.x, self.y, self.z, self.x_dot, self.y_dot, self.z_dot
+    
+    def update(self, x, y, z, x_dot=None, y_dot=None, z_dot=None):
+        self.x = x
+        self.y = y
+        self.z = z
+        if x_dot:
+            self.x_dot = x_dot
+        if y_dot:
+            self.y_dot = y_dot
+        if z_dot:
+            self.z_dot = z_dot
     
