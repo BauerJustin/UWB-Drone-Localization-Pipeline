7dac5c57d4b7fa4d24cbfd01d5203a3286caea71
BauerJustin <justin.bauer@mail.utoronto.ca>
2023-10-26 18:39:21 -0400

    Update anchor ids

--- tests/test_multilateration.py
+++ tests/test_multilateration.py
@@ -7,10 +7,10 @@ from src.devices import AnchorNetwork
 class TestMultilateration(unittest.TestCase):
     def setUp(self):
         test_anchors = {
-            "A1": {"x": 1, "y": 0, "z": 0},
-            "A2": {"x": 5, "y": 0, "z": 0},
-            "A3": {"x": 0, "y": 2, "z": 0},
-            "A4": {"x": 0, "y": 5, "z": 0}
+            "81": {"x": 1, "y": 0, "z": 0},
+            "82": {"x": 5, "y": 0, "z": 0},
+            "83": {"x": 0, "y": 2, "z": 0},
+            "84": {"x": 0, "y": 5, "z": 0}
         }
         self.anchor_network = AnchorNetwork()
         for anchor_id, pos in test_anchors.items():
@@ -19,10 +19,10 @@ class TestMultilateration(unittest.TestCase):
 
     def test_calculate_position(self):
         measurements = {
-            "A1": math.sqrt(13),
-            "A2": math.sqrt(13),
-            "A3": math.sqrt(12),
-            "A4": math.sqrt(13)
+            "81": math.sqrt(13),
+            "82": math.sqrt(13),
+            "83": math.sqrt(12),
+            "84": math.sqrt(13)
         }
         pos = self.multilateration.calculate_position(measurements)
         self.assertAlmostEqual(pos.x, 3.0, 0)
