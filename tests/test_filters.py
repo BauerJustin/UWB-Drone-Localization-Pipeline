e5869979c5df76cf6e3200ee1d39e30e7790b0cd
BauerJustin <justin.bauer@mail.utoronto.ca>
2023-12-27 14:46:53 -0700

    add ekf unit test

--- tests/test_filters.py
+++ tests/test_filters.py
@@ -112,5 +112,74 @@ class TestKalmanFilter(unittest.TestCase):
         self.assertTrue(np.allclose(self.kalman_filter.state, expected_state))
         self.assertTrue(np.allclose(self.kalman_filter.covariance, expected_covariance))
 
+class TestExtendedKalmanFilter(unittest.TestCase):
+    def setUp(self):
+        self.kalman_filter = ExtendedKalmanFilter()
+
+    def test_ekf_predict(self):
+        initial_state = np.array([1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0])
+        initial_covariance = np.eye(8)
+        transition_matrix = np.array([
+            [1.0, 0.0, 0.0, 0.0, 0.1, 0.0, 0.0, 0.0],
+            [0.0, 1.0, 0.0, 0.0, 0.0, 0.1, 0.0, 0.0],
+            [0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.1, 0.0],
+            [0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.1],
+            [0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0],
+            [0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0],
+            [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0],
+            [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0]
+        ])
+        process_noise = np.array([
+            [0.01, 0.0, 0.0, 0.0, 0.01, 0.0, 0.0, 0.0],
+            [0.0, 0.01, 0.0, 0.0, 0.0, 0.01, 0.0, 0.0],
+            [0.0, 0.0, 0.01, 0.0, 0.0, 0.0, 0.01, 0.0],
+            [0.0, 0.0, 0.0, 0.01, 0.0, 0.0, 0.0, 0.01],
+            [0.01, 0.0, 0.0, 0.0, 0.01, 0.0, 0.0, 0.0],
+            [0.0, 0.01, 0.0, 0.0, 0.0, 0.01, 0.0, 0.0],
+            [0.0, 0.0, 0.01, 0.0, 0.0, 0.0, 0.01, 0.0],
+            [0.0, 0.0, 0.0, 0.01, 0.0, 0.0, 0.0, 0.01]
+        ])
+
+        self.kalman_filter.state = initial_state
+        self.kalman_filter.covariance = initial_covariance
+        self.kalman_filter.transition_matrix = transition_matrix
+        self.kalman_filter.process_noise = process_noise
+        self.kalman_filter._kf_predict()
+
+        # Make assertions about the updated state and covariance after prediction
+        expected_state = np.dot(transition_matrix, initial_state)
+        expected_covariance = np.dot(np.dot(transition_matrix, initial_covariance), transition_matrix.T) + process_noise
+
+        self.assertTrue(np.array_equal(self.kalman_filter.state, expected_state))
+        self.assertTrue(np.array_equal(self.kalman_filter.covariance, expected_covariance))
+
+    def test_ekf_update(self):
+        initial_state = np.array([1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0])
+        initial_covariance = np.eye(8)
+        measurement = np.array([7.0, 8.0, 9.0, 10.0])
+        measurement_noise = np.array([
+            [0.1, 0.0, 0.0, 0.0],
+            [0.0, 0.1, 0.0, 0.0],
+            [0.0, 0.0, 0.1, 0.0],
+            [0.0, 0.0, 0.0, 0.1]
+        ])
+
+        self.kalman_filter.state = initial_state
+        self.kalman_filter.covariance = initial_covariance
+        observation_matrix = self.kalman_filter._calculate_jacobian()
+        self.kalman_filter.observation_matrix = observation_matrix
+        self.kalman_filter.measurement_noise = measurement_noise
+        measurement_model = self.kalman_filter._calculate_measurement_model()
+        self.kalman_filter._kf_update(measurement)
+
+        # Make assertions about the updated state and covariance after update
+        kalman_gain = np.dot(np.dot(initial_covariance, observation_matrix.T), 
+                             np.linalg.inv(np.dot(np.dot(observation_matrix, initial_covariance), observation_matrix.T) + measurement_noise))
+        expected_state = initial_state + np.dot(kalman_gain, (measurement - measurement_model))
+        expected_covariance = np.dot((np.eye(len(observation_matrix[0])) - np.dot(kalman_gain, observation_matrix)), initial_covariance)
+
+        self.assertTrue(np.allclose(self.kalman_filter.state, expected_state))
+        self.assertTrue(np.allclose(self.kalman_filter.covariance, expected_covariance))
+
 if __name__ == '__main__':
     unittest.main()
