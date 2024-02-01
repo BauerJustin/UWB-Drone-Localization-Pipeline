4d9266ab261f0453141d3517b6f1905bc45efb89
ArnavJain23 <arnav.jain@mail.utoronto.ca>
2024-02-01 11:50:48 -0500

    Add drone swarm code

--- src/control/drone_controller.py
+++ src/control/drone_controller.py
@@ -1,5 +1,7 @@
 import threading
 from djitellopy import Tello
+import time
+import socket
 
 class DroneController:
     def __init__(self):
@@ -7,8 +9,12 @@ class DroneController:
 
     def start(self):
         print(f"[Controller] Started")
-        drone_thread = threading.Thread(target=self._move_drone)
-        drone_thread.start()
+        drone_thread1 = threading.Thread(target=self._drone_swarm)
+        drone_thread2 = threading.Thread(target=self._drone_swarm)
+        drone_thread3 = threading.Thread(target=self._drone_swarm)
+        drone_thread1.start()
+        drone_thread2.start()
+        drone_thread3.start()
 
     def _move_drone(self):
         tello = Tello()
@@ -16,12 +22,21 @@ class DroneController:
         print("Battery:", tello.get_battery(), "%")
 
         tello.takeoff()
-
-        # Fly a pre-determined linear path
-        tello.move_up(20)
-        tello.move_forward(20)
-        tello.move_back(20)
-        tello.move_down(20)
+        tello.move_up(40)
+        tello.move_down(40)
+        time.sleep(30)
+        # tello.flip_right()
+        # tello.move_left(20)
+        # tello.move_forward(100)
+        # tello.move_back(100)
 
         tello.land()
         tello.end()
+
+    def _drone_swarm(self):
+        drone = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        drone.sendto('command'.encode(), 0, ('192.168.10.1', 8889))
+        drone.sendto('takeoff'.encode(), 0, ('192.168.10.1', 8889))
+        time.sleep(5)
+        drone.sendto('command'.encode(), 0, ('192.168.10.1', 8889))
+        drone.sendto('land'.encode(), 0, ('192.168.10.1', 8889))
