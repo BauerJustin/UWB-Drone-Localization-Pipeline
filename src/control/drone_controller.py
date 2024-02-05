042a7ed8af53a2bb77178afa8c0dcaa355b74cd4
nicholas3d2 <nicholas3d2@gmail.com>
2024-02-05 12:13:00 -0500

    Working drone swarm code

--- src/control/drone_controller.py
+++ src/control/drone_controller.py
@@ -10,10 +10,10 @@ class DroneController:
     def start(self):
         print(f"[Controller] Started")
         drone_thread1 = threading.Thread(target=self._drone_swarm_1)
-        drone_thread2 = threading.Thread(target=self._drone_swarm_2)
-#        drone_thread3 = threading.Thread(target=self._drone_swarm)
+#        drone_thread2 = threading.Thread(target=self._drone_swarm_2)
+#        drone_thread3 = threading.Thread(target=self._drone_swarm_3)
         drone_thread1.start()
-        drone_thread2.start()
+#        drone_thread2.start()
 #        drone_thread3.start()
 
     def _move_drone(self):
@@ -34,39 +34,38 @@ class DroneController:
         tello.end()
 
     def _drone_swarm_1(self):
-        drone2 = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)      
-        drone2.setsockopt(socket.SOL_SOCKET, 2, 'wlx00265a6b00ff'.encode("utf-8"))
-        drone2.sendto('command'.encode("utf-8"), 0, ('192.168.10.1', 8889))
+        print("Drone 1 Thread Started")
+        drone1 = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)      
+        drone1.setsockopt(socket.SOL_SOCKET, 25, 'wlx00265a6b00ff'.encode())
         
-        drone2 = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        drone2.setsockopt(socket.SOL_SOCKET, 2, 'wlx00265a6b00ff'.encode("utf-8"))
-        drone2.sendto('takeoff'.encode("utf-8"), 0, ('192.168.10.1', 8889))
-        time.sleep(5)
-
-        drone2 = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        drone2.setsockopt(socket.SOL_SOCKET, 2, 'wlx00265a6b00ff'.encode("utf-8"))        
-        drone2.sendto('command'.encode("utf-8"), 0, ('192.168.10.1', 8889))
+        drone1.sendto('command'.encode("utf-8"), 0, ('192.168.10.1', 8889))
+        drone1.sendto('takeoff'.encode("utf-8"), 0, ('192.168.10.1', 8889))
+#        time.sleep(5)
         
-        drone2 = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        drone2.setsockopt(socket.SOL_SOCKET, 2, 'wlx00265a6b00ff'.encode("utf-8"))
-        drone2.sendto('land'.encode("utf-8"), 0, ('192.168.10.1', 8889))
+        drone1.sendto('command'.encode("utf-8"), 0, ('192.168.10.1', 8889))
+        drone1.sendto('land'.encode("utf-8"), 0, ('192.168.10.1', 8889))
     
     def _drone_swarm_2(self):
-        drone = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        drone.setsockopt(socket.SOL_SOCKET, 2, 'wlx00265a6afa9d'.encode("utf-8"))
-        drone.sendto('command'.encode("utf-8"), 0, ('192.168.10.1', 8889))
+        print("Drone 2 Thread Started")
+        drone2 = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        drone2.setsockopt(socket.SOL_SOCKET, 25, 'wlx00265a6afa9d'.encode())
         
-        drone = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        drone.setsockopt(socket.SOL_SOCKET, 2, 'wlx00265a6afa9d'.encode("utf-8"))
-        drone.sendto('takeoff'.encode("utf-8"), 0, ('192.168.10.1', 8889))
-        time.sleep(5)
+        drone2.sendto('command'.encode("utf-8"), 0, ('192.168.10.1', 8889))
+        drone2.sendto('takeoff'.encode("utf-8"), 0, ('192.168.10.1', 8889))
+#        time.sleep(5)
 
-        drone = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        drone.setsockopt(socket.SOL_SOCKET, 2, 'wlx00265a6afa9d'.encode("utf-8"))        
-        drone.sendto('command'.encode("utf-8"), 0, ('192.168.10.1', 8889))
-        
-        drone = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        drone.setsockopt(socket.SOL_SOCKET, 2, 'wlx00265a6afa9d'.encode("utf-8"))
-        drone.sendto('land'.encode("utf-8"), 0, ('192.168.10.1', 8889))
-        
+        drone2.sendto('command'.encode("utf-8"), 0, ('192.168.10.1', 8889))
+        drone2.sendto('land'.encode("utf-8"), 0, ('192.168.10.1', 8889))
     
+    def _drone_swarm_3(self):
+        print("Drone 3 Thread Started")
+        drone2 = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        drone2.setsockopt(socket.SOL_SOCKET, 25, 'wlp3s0'.encode())
+        
+        drone2.sendto('command'.encode("utf-8"), 0, ('192.168.10.1', 8889))
+        drone2.sendto('takeoff'.encode("utf-8"), 0, ('192.168.10.1', 8889))
+#        time.sleep(5)
+
+        drone2.sendto('command'.encode("utf-8"), 0, ('192.168.10.1', 8889))
+        drone2.sendto('land'.encode("utf-8"), 0, ('192.168.10.1', 8889))
+
