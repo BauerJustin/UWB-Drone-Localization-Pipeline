19c4ff369f50f91e44b5d60431bd5f45e7d35e0e
nicholas3d2 <nicholas3d2@gmail.com>
2024-02-08 12:57:24 -0500

    Update drone swarm code

--- src/control/drone_controller.py
+++ src/control/drone_controller.py
@@ -5,18 +5,18 @@ import socket
 
 class DroneController:
     def __init__(self):
-        pass
+        self.response = None
 
     def start(self):
         print(f"[Controller] Started")
-        drone_thread1 = threading.Thread(target=self._drone_swarm_1)
-#        drone_thread2 = threading.Thread(target=self._drone_swarm_2)
-#        drone_thread3 = threading.Thread(target=self._drone_swarm_3)
+        drone_thread1 = threading.Thread(target=self._drone_swarm('wlx00265a6b00ff', 1))
+        drone_thread2 = threading.Thread(target=self._drone_swarm('wlx00265a6afa9d', 2))
+        drone_thread3 = threading.Thread(target=self._drone_swarm('wlp3s0', 3))
+
         drone_thread1.start()
-        drone_thread1.join()
-        
-#        drone_thread2.start()
-#        drone_thread3.start()
+        drone_thread2.start()
+        drone_thread3.start()
+
 
     def _move_drone(self):
         tello = Tello()
@@ -35,25 +35,40 @@ class DroneController:
         tello.land()
         tello.end()
 
-    def _drone_swarm_1(self):
-        print("Drone 1 Thread Started")
-        drone1 = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)      
-        drone1.setsockopt(socket.SOL_SOCKET, 25, 'wlx00265a6b00ff'.encode())
+    def _get_response(self, socket, command):
+        self.response = socket.recv(3000)
+        print(f'Command: {command}; socket response: {self.response}')
+        if self.response is None:
+            self.response = "none response"
+        else:
+            self.response = self.response.decode("utf-8")
+        print(f'Command: {command}; decoded response: {self.response}')
+        self.response = None
+        
+    def _drone_swarm(self, wifi, num):
+        print(f'Drone {num} Thread Started')
+        drone1 = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        drone1.setsockopt(socket.SOL_SOCKET, 25, wifi.encode())
         
         drone1.sendto('command'.encode("utf-8"), 0, ('192.168.10.1', 8889))
         drone1.sendto('takeoff'.encode("utf-8"), 0, ('192.168.10.1', 8889))
-        temp = drone1.recvfrom(1024)
-        print(temp)
-        time.sleep(2)
-        drone1.sendto('command'.encode("utf-8"), 0, ('192.168.10.1', 8889))
-        drone1.sendto('forward 100'.encode("utf-8"), 0, ('192.168.10.1', 8889))
+#        self._get_response(drone1, "takeoff")
         
         drone1.sendto('command'.encode("utf-8"), 0, ('192.168.10.1', 8889))
+        drone1.sendto('flip f'.encode("utf-8"), 0, ('192.168.10.1', 8889))
+#        self._get_response(drone1, "forward 50")
+
+        drone1.sendto('command'.encode("utf-8"), 0, ('192.168.10.1', 8889))
+        drone1.sendto('flip b'.encode("utf-8"), 0, ('192.168.10.1', 8889))
+#        self._get_response(drone1, "back 50")
+
+#        time.sleep(2)       
+        drone1.sendto('command'.encode("utf-8"), 0, ('192.168.10.1', 8889))
         drone1.sendto('land'.encode("utf-8"), 0, ('192.168.10.1', 8889))
-        temp2 = drone1.recvfrom(1024)
-        print(temp2)
-        time.sleep(2)
-        print("Drone 1 Thread Stopped")
+#        self._get_response(drone1, "land")
+
+#        time.sleep(2)
+        print(f'Drone {num} Thread Stopped')
     
     def _drone_swarm_2(self):
         print("Drone 2 Thread Started")
