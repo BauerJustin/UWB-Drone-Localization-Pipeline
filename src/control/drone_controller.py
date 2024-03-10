50b570174af5af9ae98708e9a481327cedcf0d4f
BauerJustin <justin.bauer@mail.utoronto.ca>
2024-03-10 13:36:22 -0400

    Add control flags

--- src/control/drone_controller.py
+++ src/control/drone_controller.py
@@ -1,27 +1,36 @@
+from src import constants as const
 from djitellopy import Tello
 import time
 import socket
 import multiprocessing
 
+
 class DroneController:
     def __init__(self):
-        self.response = None
-
-        self.drone_process_1 = multiprocessing.Process(target=self._drone_swarm_test, args=('wlx00265a6b00ff', 1))
-        self.drone_process_2 = multiprocessing.Process(target=self._drone_swarm_test, args=('wlx00265a6afa9d', 2))
-        self.drone_process_3 = multiprocessing.Process(target=self._drone_swarm_test,  args=('wlp3s0', 3))
-
-        # self.drone_process_1 = multiprocessing.Process(target=self._drone_swarm)
-        # self.drone_process_2 = multiprocessing.Process(target=self._drone_swarm_2)
-        # self.drone_process_3 = multiprocessing.Process(target=self._drone_swarm_3)
+        if const.SWARM_MODE:
+            self.drone_network = const.DRONE_NETWORK
+            drone_wifi_ids = const.DRONE_WIFI_IDS
+            self.drone_processes = []
+            for i, wifi_id in enumerate(drone_wifi_ids):
+                if const.DRONE_CALIBRATION:
+                    self.drone_processes.append(multiprocessing.Process(target=self._drone_swarm_calibration, args=(wifi_id, i+1)))
+                else:
+                    self.drone_processes.append(multiprocessing.Process(target=self._drone_swarm, args=(wifi_id, i+1)))
 
     def start(self):
         print(f"[Controller] Started")
-        self.drone_process_1.start()
-        self.drone_process_2.start()
-        self.drone_process_3.start()
+        if const.SWARM_MODE:
+            print(f"[Controller] Swarm mode activated")
+            for process in self.drone_processes:
+                process.start()
+        else:
+            print(f"[Controller] Single drone activated")
+            if const.DRONE_CALIBRATION:
+                self._drone_single_calibration()
+            else:
+                self._drone_single()
 
-    def _move_drone(self):
+    def _drone_single(self):
         tello = Tello()
         tello.connect()
         print("Battery:", tello.get_battery(), "%")
@@ -30,156 +39,49 @@ class DroneController:
         tello.land()
         tello.end()
 
-    def _get_response(self, socket, command):
-        self.response = socket.recv(3000)
-        if self.response is None:
-            self.response = "none response"
-        else:
-            self.response = self.response.decode("utf-8")
-            self.response = self.response.rstrip("\r\n")
-        print(f'Command: {command} \n\t decoded response: {self.response}')
-        self.response = None
+    def _drone_single_calibration(self):
+        pass
 
-    def _drone_tag_calibration(self, wifi, num):
-        print(f'Drone-tag {num} calibration started')
-        drone1 = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        drone1.setsockopt(socket.SOL_SOCKET, 25, wifi.encode())
-        
-        drone1.sendto('command'.encode("utf-8"), 0, ('192.168.10.1', 8889))
-        drone1.sendto('takeoff'.encode("utf-8"), 0, ('192.168.10.1', 8889))
-        drone1.sendto('land'.encode("utf-8"), 0, ('192.168.10.1', 8889))
-        print(f'Drone-tag {num} calibration stopped')
-
-    def _drone_swarm_test(self, wifi, num):
+    def _drone_swarm(self, wifi, num):
         print(f'Drone {num} Thread Started')
-        drone1 = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        drone1.setsockopt(socket.SOL_SOCKET, 25, wifi.encode())
+        drone_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        drone_socket.setsockopt(socket.SOL_SOCKET, 25, wifi.encode())
         
-        drone1.sendto('command'.encode("utf-8"), 0, ('192.168.10.1', 8889))
-        drone1.sendto('takeoff'.encode("utf-8"), 0, ('192.168.10.1', 8889))
-        time.sleep(2) # Wait for all 3 drones to takeoff
+        self._drone_socket_send(drone_socket, 'command')
+        self._drone_socket_send(drone_socket, 'takeoff')
 
-        command = "speed 10"
-        drone1.sendto(command.encode("utf-8"), 0, ('192.168.10.1', 8889))
-        
-        for i in range(3):
-            command = f'rc 0 30 0 0'
-            drone1.sendto(command.encode("utf-8"), 0, ('192.168.10.1', 8889))
+        time.sleep(2)   # Wait for all 3 drones to takeoff
+
+        self._drone_socket_send(drone_socket, 'speed 10')
+ 
+        for _ in range(3):
+            self._drone_socket_send(drone_socket, 'rc 0 30 0 0')
             time.sleep(2)
-            command = "rc 0 0 0 0"
-            drone1.sendto(command.encode("utf-8"), 0, ('192.168.10.1', 8889))
+            self._drone_socket_send(drone_socket, 'rc 0 0 0 0')
             time.sleep(1)
         time.sleep(5)
 
-        for i in range(3):
-            command = f'rc 0 -30 0 0'
-            drone1.sendto(command.encode("utf-8"), 0, ('192.168.10.1', 8889))
+        for _ in range(3):
+            self._drone_socket_send(drone_socket, 'rc 0 -30 0 0')
             time.sleep(2)
-            command = "rc 0 0 0 0"
-            drone1.sendto(command.encode("utf-8"), 0, ('192.168.10.1', 8889))
+            self._drone_socket_send(drone_socket, 'rc 0 0 0 0')
             time.sleep(1)
         time.sleep(5)
 
-        drone1.sendto('land'.encode("utf-8"), 0, ('192.168.10.1', 8889))
+        self._drone_socket_send(drone_socket, 'land')
 
         print(f'Drone {num} Thread Stopped')
-    
-    def _drone_swarm(self):
-        print(f'Drone 1 Thread Started')
-        drone1 = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        drone1.setsockopt(socket.SOL_SOCKET, 25, 'wlx00265a6b00ff'.encode())
-        
-        drone1.sendto('command'.encode("utf-8"), 0, ('192.168.10.1', 8889))
-        drone1.sendto('takeoff'.encode("utf-8"), 0, ('192.168.10.1', 8889))
-        time.sleep(2) # Wait for all 3 drones to takeoff
 
-        command = "speed 10"
-        drone1.sendto(command.encode("utf-8"), 0, ('192.168.10.1', 8889))
-        
-        for i in range(3):
-            command = f'rc 0 30 0 0'
-            drone1.sendto(command.encode("utf-8"), 0, ('192.168.10.1', 8889))
-            time.sleep(2)
-            command = "rc 0 0 0 0"
-            drone1.sendto(command.encode("utf-8"), 0, ('192.168.10.1', 8889))
-            time.sleep(1)
-        time.sleep(5)
+    def _drone_swarm_calibration(self, wifi, num):
+        print(f'Drone-tag {num} calibration started')
+        drone_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        drone_socket.setsockopt(socket.SOL_SOCKET, 25, wifi.encode())
 
-        for i in range(3):
-            command = f'rc 0 -30 0 0'
-            drone1.sendto(command.encode("utf-8"), 0, ('192.168.10.1', 8889))
-            time.sleep(2)
-            command = "rc 0 0 0 0"
-            drone1.sendto(command.encode("utf-8"), 0, ('192.168.10.1', 8889))
-            time.sleep(1)
-        time.sleep(5)
+        self._drone_socket_send(drone_socket, 'command')
+        self._drone_socket_send(drone_socket, 'takeoff')
+        self._drone_socket_send(drone_socket, 'land')
 
-        drone1.sendto('land'.encode("utf-8"), 0, ('192.168.10.1', 8889))
+        print(f'Drone-tag {num} calibration stopped')
 
-        print(f'Drone 1 Thread Stopped')
-    
-    def _drone_swarm_2(self):
-        print("Drone 2 Thread Started")
-        drone2 = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        drone2.setsockopt(socket.SOL_SOCKET, 25, 'wlx00265a6afa9d'.encode())
-        
-        drone2.sendto('command'.encode("utf-8"), 0, ('192.168.10.1', 8889))
-        drone2.sendto('takeoff'.encode("utf-8"), 0, ('192.168.10.1', 8889))
-        time.sleep(2) # Wait for all 3 drones to takeoff
-
-        command = "speed 10"
-        drone2.sendto(command.encode("utf-8"), 0, ('192.168.10.1', 8889))
-
-        for i in range(3):
-            command = f'rc 0 30 0 0'
-            drone2.sendto(command.encode("utf-8"), 0, ('192.168.10.1', 8889))
-            time.sleep(2)
-            command = "rc 0 0 0 0"
-            drone2.sendto(command.encode("utf-8"), 0, ('192.168.10.1', 8889))
-            time.sleep(1)
-        time.sleep(5)
-
-        for i in range(3):
-            command = f'rc 0 -30 0 0'
-            drone2.sendto(command.encode("utf-8"), 0, ('192.168.10.1', 8889))
-            time.sleep(2)
-            command = "rc 0 0 0 0"
-            drone2.sendto(command.encode("utf-8"), 0, ('192.168.10.1', 8889))
-            time.sleep(1)
-        time.sleep(5)
-
-        drone2.sendto('land'.encode("utf-8"), 0, ('192.168.10.1', 8889))
-        print("Drone 2 Thread Stopped")
-    
-    def _drone_swarm_3(self):
-        print("Drone 3 Thread Started")
-        drone3 = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        drone3.setsockopt(socket.SOL_SOCKET, 25, 'wlp3s0'.encode())
-        
-        drone3.sendto('command'.encode("utf-8"), 0, ('192.168.10.1', 8889))
-        drone3.sendto('takeoff'.encode("utf-8"), 0, ('192.168.10.1', 8889))
-        time.sleep(2) # Wait for all 3 drones to takeoff
-
-        command = "speed 10"
-        drone3.sendto(command.encode("utf-8"), 0, ('192.168.10.1', 8889))
-
-        for i in range(3):
-            command = f'rc 0 30 0 0'
-            drone3.sendto(command.encode("utf-8"), 0, ('192.168.10.1', 8889))
-            time.sleep(2)
-            command = "rc 0 0 0 0"
-            drone3.sendto(command.encode("utf-8"), 0, ('192.168.10.1', 8889))
-            time.sleep(1)
-        time.sleep(5)
-
-        for i in range(3):
-            command = f'rc 0 -30 0 0'
-            drone3.sendto(command.encode("utf-8"), 0, ('192.168.10.1', 8889))
-            time.sleep(2)
-            command = "rc 0 0 0 0"
-            drone3.sendto(command.encode("utf-8"), 0, ('192.168.10.1', 8889))
-            time.sleep(1)
-        time.sleep(5)
-
-        drone3.sendto('land'.encode("utf-8"), 0, ('192.168.10.1', 8889))
-        print("Drone 3 Thread Stopped")
+    def _drone_socket_send(self, drone_socket, command):
+        drone_socket.sendto(command.encode("utf-8"), 0, self.drone_network)
